4d1900235efc08f07cfc45f8cd523683b7cfc7de
kjm9198 <kgm9197@gmail.com>
2024-01-29 01:49:44 +0100

    Made a lot of progress - made the navigation bar work - The Logo on the left side

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const NavBar = () => {
+  const [nav, navSet] = useState(false);
+
+  const links = [
+    {
+      id: 1,
+      link: "home",
+    },
+    {
+      id: 2,
+      link: "about",
+    },
+    {
+      id: 3,
+      link: "projects",
+    },
+    {
+      id: 4,
+      link: "experience",
+    },
+    {
+      id: 5,
+      link: "contact",
+    },
+  ];
+
+  return (
+    <div
+      className="flex justify-between items-center w-full h-20
+    px-4 text-white bg-blue-300 fixed"
+    >
+      <div>
+        <h1 className="text-5xl font-signature ml-2">Kajotem</h1>
+      </div>
+
+      <ul className="hidden md:flex">
+        {links.map(({ id, link }) => (
+          <li
+            key={id}
+            className="px-4 cursor-pointer capitalize font-medium text-white-50
+                    hover:scale-105 duration-300"
+          >
+            {link}
+          </li>
+        ))}
+      </ul>
+
+      <div onClick={() => navSet(!nav)} className="cursor-pointer pr-4 z-10 md:hidden">
+        {nav ? (
+          <FaTimes className="text-3xl" />
+        ) : (
+          <FaBars className="text-3xl" />
+        )}
+      </div>
+
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-blue-300 to-blue-600  ">
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl font-medium text-white-50"
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default NavBar;
