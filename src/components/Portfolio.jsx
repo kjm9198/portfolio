6811a53bbc2347b5a0ba3a186992d0fb0f15bc26
kjm9198 <kgm9197@gmail.com>
2024-02-10 20:58:14 +0100

    Made the 4 buttons appear and work

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,43 +1,46 @@
 import React from "react";
 
 function Portfolio() {
-    return (
-        <div id="portfolio" className="h-screen w-full bg-gradient-to-b from-blue-500 to-blue-600">
-            <h1>Portfolio</h1>
-            <div className="container">
-                <div className="card">
-                    <div className="box">
-                        <img alt=""/>
-                        <div className="text">Project 1</div>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="box">
-                        <img alt=""/>
-                        <div className="text">Project 2</div>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="box">
-                        <img alt=""/>
-                        <div className="text">Project 3</div>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="box">
-                        <img alt=""/>
-                        <div className="text">Project 4</div>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="box">
-                        <img alt=""/>
-                        <div className="text">Project 5</div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div
+      id="portfolio"
+      className="h-screen w-full bg-gradient-to-b from-blue-500 to-blue-600 flex justify-center items-center"
+    >
+      <h1 className="text-center w-full">Portfolio</h1>
+      <div className="container grid grid-cols-3 gap-4">
+        <div className="card">
+          <div className="box">
+            <img alt="" />
+            <div className="text border-2 border-black p-2">Project 1</div>
+          </div>
         </div>
-    );
+        <div className="card">
+          <div className="box">
+            <img alt="" />
+            <div className="text border-2 border-black p-2">Project 2</div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="box">
+            <img alt="" />
+            <div className="text border-2 border-black p-2">Project 3</div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="box">
+            <img alt="" />
+            <div className="text border-2 border-black p-2">Project 4</div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="box">
+            <img alt="" />
+            <div className="text border-2 border-black p-2">Project 5</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Portfolio;
