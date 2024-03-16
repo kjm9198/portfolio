d89bfaeb2753dcbf191695b75beebc599257c0a3
Michał Bożek <kgm9197@gmail.com>
2024-03-16 01:45:28 +0100

    Added how the projects look like

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,6 +7,33 @@ import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
 import reactWeather from "../assets/portfolio/reactWeather.jpg";
 
 const Portfolio = () => {
+  const portfolios = [
+    {
+      id: 1,
+      src: arrayDestruct,
+    },
+    {
+      id: 2,
+      src: installNode,
+    },
+    {
+      id: 3,
+      src: navbar,
+    },
+    {
+      id: 4,
+      src: reactParallax,
+    },
+    {
+      id: 5,
+      src: reactSmooth,
+    },
+    {
+      id: 6,
+      src: reactWeather,
+    },
+  ];
+
   return (
     <div
       name="portfolio"
@@ -19,12 +46,27 @@ const Portfolio = () => {
           </p>
           <p className="py-6">Check my work right here</p>
         </div>
-
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          <div className="shadow-md shadow-blue-900 rounded-lg project-div">
-            <img src={arrayDestruct} alt="arrayDestruct "/>
-            <p>Array Destructuring</p>
-          </div>
+          {portfolios.map(({ id, src }) => (
+            <div
+              key={id}
+              className="shadow-md shadow-blue-900 rounded-lg project-div"
+            >
+              <img
+                src={src}
+                alt="arrayDestruct "
+                className="rounded-md duration-200 hover:scale-100"
+              />
+              <div className="flex items-center justify-center ">
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Demo
+                </button>
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Code
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
